d88aaa388938089c6d9d647549012ba332e71e06
ccbirk <ccbirkenstock@gmail.com>
2023-10-05 21:21:08 +0200

    betty checked

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - print quote
- *
- * Description: prints Dors's quote
- *
+ * main - print a statement
+ * Description: prints some text
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
