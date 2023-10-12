970d47e4eebb07f674ea5ca727692cd47c4e5352
ccbirk <ccbirkenstock@gmail.com>
2023-10-12 22:10:51 +0200

    ADVANCED TASKS

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - Prints number
+ *
+ * @n: integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_number(int n)
+{
+unsigned int num = n;
+
+if (n < 0)
+{
+_putchar(' ');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+
+_putchar((num % 10) + '0');
+}
