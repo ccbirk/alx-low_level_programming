8062ce5e256e7017e30f16c754a4922202522a7e
ccbirk <ccbirkenstock@gmail.com>
2023-10-12 23:57:21 +0200

    ammended 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,28 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_line - Draws a line in the terminal
  *
  * @n: Number of characters
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
-void print_line(int n) {
-  if (n <= 0) {
-    _putchar('\n');
-  } else {
-    for (int i = 0; i < n; i++) {
-      _putchar('_');
-    }
-    _putchar('\n');
-  }
+void print_line(int n)
+{
+int ln;
+
+if (n <= 0)
+{
+_putchar('\n');
 }
 
+else
+{
+for (ln = 1; ln <= n; ln++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
