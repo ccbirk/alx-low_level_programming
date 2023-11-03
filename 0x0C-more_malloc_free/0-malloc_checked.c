9bf72fd3f4659f89a487171be2b80fdf4c31f872
ccbirk <ccbirkenstock@gmail.com>
2023-11-03 04:59:38 +0200

    checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,20 +4,19 @@
 
 /**
  * malloc_checked - Allocates memory
- * 
+ *
  * @b: the size to allocate
- * 
+ *
  * Return - Nothing
- * 
+ * 0-malloc_checked -  checks for value
 */
 
-void *malloc_checked(unsigned int b) 
+void *malloc_checked(unsigned int b)
 {
-    void *p;
-
-    p = malloc(b);
-    if (p == NULL)
-        exit(98);
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
 
     return (p);
 }
