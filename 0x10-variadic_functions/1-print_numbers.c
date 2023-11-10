4ce043fc41525609f006d5bc63916a4dbeeb110c
ccbirk <ccbirkenstock@gmail.com>
2023-11-10 12:13:07 +0200

    modded before betty test

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,32 +1,30 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * print_numbers - prints @n integer args using a separator in between
- * @n: number of args that will get fed
+ * print_numbers - prints @n integer args using a separator in between @n: number of args that will get fed
  * @separator: a string(char pointer) to separate @n args
- * check if @separator is NULL or not
+ * check if the @separator is NULL or not
  * if not NULL prints @n args followed by @separator
- * while i(counter) is less than (@n - 1) and once i < n print a new line
- * otherwise print @n args without @separator and end the full print using
- * new line
+ * while i(counter) is less than (@n - 1) and once i < n print a new line otherwise print @n args without @separator and end the full print using anew line
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list list;
+	unsigned int i = 0;
+	int Holder = 0;
 
-	va_start(list, n);
+	va_list args;
+
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
-			printf("%d", va_arg(list, int));
-		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
-		else
-			printf("%s%d", separator, va_arg(list, int));
+	Holder = va_arg(args, int);
+	printf("%d", Holder);
+
+	if (i < (n - 1) && separator != NULL)
+		printf("%s", separator);
 	}
-
-	va_end(list);
-
-	printf("\n");
+	va_end(args);
+	putchar('\n');
 }
