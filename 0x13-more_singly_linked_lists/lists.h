cfd709fa18967a1ed7a849821fa633ecc0bc95b2
ccbirk <ccbirkenstock@gmail.com>
2023-11-21 21:17:05 +0200

    betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 /**
  * struct listint_s - singly linked list
@@ -29,15 +29,12 @@ void free_listint(listint_t *head);
 /*Write a function that frees a listint_t list.*/
 void free_listint2(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-
-/*
-*Write a function that returns the sum of all the data (n) of
-*a listint_t linked list.
-*int sum_listint(listint_t *head);
-*/
-
-/*Write a function that inserts a new node at a given position.*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
 
+size_t free_listint_safe(listint_t **h);
+
+listint_t *find_listint_loop(listint_t *head);
 #endif
